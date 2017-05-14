37469cea57bf6c344776635ca217a7cfd3b4fec5
BraulioRM <ramirez.braulioa@gmail.com>
2017-05-13 20:21:25 -0500

    Se agregan librerias de sdkfb desde xcode, se agrega lin sdkfb y se agrega boton, botón aún no se ve la imagen

--- src/App.js
+++ src/App.js
@@ -18,12 +18,15 @@ import {getArtists} from './Api-client'
 
 
 import {Scene, Router} from 'react-native-router-flux';
+
+import LoginView from './LoginView'
 import HomeView from './HomeView'
 import ArtistDetailView from './ArtistDetailView'
 
 class MusicList extends React.Component {
   render() {
     return <Router>
+      <Scene key="login" component={LoginView} hideNavBar/>
       <Scene key="root">
         <Scene key="home" component={HomeView} hideNavBar/>
         <Scene key="artistDetail" component={ArtistDetailView} hideNavBar = {false} />
